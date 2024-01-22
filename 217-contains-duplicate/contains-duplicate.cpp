9d673f047d648e9371ea4eccda762db67ab8ed43
Sachin <58112222+SachinBore@users.noreply.github.com>
2024-01-22 20:52:46 +0530

    Time: 96 ms (71.78%) | Memory: 73.2 MB (10.72%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -6,11 +6,9 @@ public:
         int n = nums.size();
         for(int i=0; i<n; i++) {
             mp[nums[i]]++;
+            if(mp[nums[i]] == 2) return true;
         }
 
-        for(auto it : mp) {
-            if(it.second >= 2) return true;
-        }
         return false;
     }
 };
