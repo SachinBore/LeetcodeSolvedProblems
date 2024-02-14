c5cc33c51baef35611f677886f480e200431f98a
Sachin <58112222+SachinBore@users.noreply.github.com>
2024-02-14 18:21:57 +0530

    Time: 3 ms (98.93%) | Memory: 18.2 MB (46.17%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,20 +1,42 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& arr) {
+
         int n = arr.size();
-	vector<int> res;
+        int cnt1 = 0, cnt2 = 0;
+	    int el1 = INT_MIN, el2 = INT_MIN;
 
-	unordered_map<int, int> mpp;
-	int mini = int(n / 3) + 1;
-	
-	for(int i=0; i<n; i++) {
-		mpp[arr[i]]++;
+	    for (int i = 0; i < n; i++) {
+		    if (cnt1 == 0 && arr[i] != el2) {
+			    cnt1 = 1;
+			    el1 = arr[i];
+		    }
+		    else if (cnt2 == 0 && arr[i] != el1) {
+			    cnt2 = 1;
+			    el2 = arr[i];
+		    }
+		    else if (el1 == arr[i]) cnt1++;
+		    else if (el2 == arr[i]) cnt2++;
+		    else {
+			    cnt1--;
+			    cnt2--;
+		    }
+	    }
 
-		if(mpp[arr[i]] == mini) res.push_back(arr[i]);
+	    vector<int> res;
+	    cnt1 = 0, cnt2 = 0;
+	    int mini = n / 3 + 1;
 
-		if(res.size() == 2) break;
-	}
-    sort(res.begin(), res.end());
-	return res;
+	    for (int i = 0; i < n; i++) {
+		    if (el1 == arr[i]) cnt1++;
+		    if (el2 == arr[i]) cnt2++;
+	    }
+
+	    if (cnt1 >= mini) res.push_back(el1);
+	    if (cnt2 >= mini) res.push_back(el2);
+
+	    sort(res.begin(), res.end());
+
+        return res;
     }
 };
