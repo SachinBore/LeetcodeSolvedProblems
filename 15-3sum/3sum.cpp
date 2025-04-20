155ddf49016569666b038cebf0820e0c206da2ee
Sachin <58112222+SachinBore@users.noreply.github.com>
2025-04-20 18:14:24 +0530

    Time: 45 ms (78.14%) | Memory: 29.2 MB (51.70%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& arr) {
-        int n = arr.size();
-        sort(arr.begin(), arr.end());
-
+    vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<n; i++) {
+            if(i>0 && nums[i] == nums[i-1]) continue;
 
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && arr[i] == arr[i - 1])
-                continue;
-            int j = i + 1;
-            int k = n - 1;
-            while (j < k) {
-                int sum = arr[i] + arr[j] + arr[k];
-                if (sum < 0) {
-                    j++;
-                } else if (sum > 0) {
-                    k--;
-                } else {
-                    vector<int> tmp = {arr[i], arr[j], arr[k]};
-                    ans.push_back(tmp);
-                    j++;
-                    k--;
-                    while (j<k && arr[j] == arr[j - 1])
-                        j++;
-                    while (j<k && arr[k] == arr[k + 1])
-                        k--;
-                }
+            int left = i+1, right = n-1;
+            while(left < right) {
+                int total = nums[i]+nums[left]+nums[right];
+                if(total == 0) {
+                    ans.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
+
+                    while(left < n && nums[left] == nums[left-1]) left++;
+                    while(right > 0 && nums[right] == nums[right+1]) right--;
+                } else if(total < 0) {
+                    left++;
+                } else right--;
             }
         }
 
