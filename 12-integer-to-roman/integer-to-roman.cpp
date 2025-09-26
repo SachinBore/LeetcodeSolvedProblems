c1c3ab7df8eec1bf53cc4ddcdd12e18cf25b3daa
Sachin <58112222+SachinBore@users.noreply.github.com>
2025-09-27 00:23:20 +0530

    Time: 7 ms (28.76%) | Memory: 12.2 MB (45.23%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
-    void append(string& ans, int times, string ch) {
-        while (times--) {
-            ans += ch;
-        }
-    }
-
     string intToRoman(int num) {
 
-        string ans;
-        unordered_map<int, string> mp = {
-            {1, "I"},   {4, "IV"},   {5, "V"},   {9, "IX"},  {10, "X"},
-            {40, "XL"}, {50, "L"},   {90, "XC"},  {100, "C"}, {400, "CD"},
-            {500, "D"}, {900, "CM"}, {1000, "M"}};
+        vector<int> val = {1000, 900, 500, 400, 100, 90, 50,
+                           40,   10,  9,   5,   4,   1};
 
-        vector<int> segments = {1000, 900, 500, 400, 100, 90, 50,
-                                40,   10,  9,   5,   4,   1};
-        for (int i = 0; i < segments.size(); i++) {
-            int rem = num / segments[i];
-            append(ans, rem, mp[segments[i]]);
-            num -= rem * segments[i];
+        vector<string> sym = {"M",  "CM", "D",  "CD", "C",  "XC", "L",
+                              "XL", "X",  "IX", "V",  "IV", "I"};
+
+        string ans;
+        for(int i=0; i<val.size() && num>0; i++) {
+            int cnt = num/val[i];
+            num %= val[i];
+            while(cnt--) ans += sym[i];
         }
 
         return ans;
