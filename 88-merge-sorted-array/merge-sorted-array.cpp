cd32ed4e13be20c5e263d09aae125feccee25e5b
Sachin <58112222+SachinBore@users.noreply.github.com>
2025-09-28 22:13:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (70.74%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,34 +1,17 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> ans;
-        int i=0, j=0;
-        while(i<m && j<n) {
-            if(nums1[i] < nums2[j]) {
-                ans.push_back(nums1[i]);
-                i++;
-            } else if(nums1[i] > nums2[j]) {
-                ans.push_back(nums2[j]);
-                j++;
+        int i=m-1, j=n-1;
+        int indx = m+n-1;
+
+        while(j>=0) {
+            if(i>=0 && nums1[i] >= nums2[j]) {
+                nums1[indx] = nums1[i];
+                i--; indx--;
             } else {
-                ans.push_back(nums1[i]);
-                ans.push_back(nums2[j]);
-                i++;j++;
+                nums1[indx] = nums2[j];
+                j--; indx--;
             }
-        }
-
-        while(i<m) {
-            ans.push_back(nums1[i]);
-            i++;
-        }
-
-        while(j<n) {
-            ans.push_back(nums2[j]);
-            j++;
-        }
-
-        for(int i=0; i<ans.size(); i++) {
-            nums1[i] = ans[i];
         }
     }
 };
