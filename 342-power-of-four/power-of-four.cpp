2c1e321abfdd6de145377f0108bd31596fceb9fb
Sachin <58112222+SachinBore@users.noreply.github.com>
2025-09-30 23:04:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (52.47%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        // using bit masking
         if(n<=0) return false;
-        int mask = 0b01010101010101010101010101010101;
-        return (n&(n-1)) == 0 && (n|mask) == mask;
+
+        // %3 trick
+        return ((n&(n-1)) == 0) && (n%3 == 1);
     }
 };
