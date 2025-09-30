3c84b5a6ad99f5d9d8eb4c6ac12cd006e0a9dd4d
Sachin <58112222+SachinBore@users.noreply.github.com>
2025-09-30 23:20:31 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (97.41%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        int n1= nums1.size();
+        int n1 = nums1.size();
         int n2 = nums2.size();
 
         sort(nums1.begin(), nums1.end());
         sort(nums2.begin(), nums2.end());
 
-        int i=0, j=0;
         vector<int> ans;
-        while(i<n1 && j<n2) {
-            if(nums1[i] == nums2[j]) {
-                if(ans.size() == 0 || ans.back() != nums1[i]) {
+        int i = 0, j = 0;
+        while (i < n1 && j < n2) {
+            if (ans.size() == 0) {
+                if (nums1[i] == nums2[j]) {
                     ans.push_back(nums1[i]);
-                }
+                    i++; j++;
+                } else if(nums1[i] < nums2[j]) {
+                    i++;
+                } else j++;
+
+                continue;
+            }
+
+            if(nums1[i] == nums2[j] && ans.back() != nums1[i]) {
+                ans.push_back(nums1[i]);
                 i++; j++;
             } else if(nums1[i] < nums2[j]) {
-                i++;
+                    i++;
             } else j++;
         }
 
